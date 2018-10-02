8758e1ea5be55053ac67e68866fdff1965153b68
bmagic <baptiste.routhieau@gmail.com>
2018-10-02 11:33:13 +0200

    Big update with config move.

--- config/config.js
+++ config/config.js
@@ -1,33 +1,53 @@
 let config = {
   title: 'Atlantic LAN 2018',
+  logo: `/static/img/logo-183x100.png`,
   description: 'Retrouvez l\'Atlantic LAN très prochainement',
-  image: `${process.env.BASE_URL}/static/img/logo-365x200.png`,
+  metaImage: `${process.env.BASE_URL}/static/img/logo-365x200.png`,
   home: {
-    banner: '/static/img/bg-banner-al2018.jpg'
+    banner: '/static/img/bg-banner-al2018.jpg',
+    twitterAccount: 'AtlanticLan17'
   },
   news: {
+    active: true,
     title: 'Actualités',
     description: 'Retrouvez toutes les actualités de l\'Atlantic LAN 2018'
   },
   recruit: {
+    active: false,
     title: 'Recrutement',
-    description: 'Devenir bénévole pour l\'Atlantic LAN 2018, c\'est par ici !!!'
+    description: 'Devenir bénévole pour l\'Atlantic LAN 2018, c\'est par ici !!!',
+    formUrl: 'https://goo.gl/forms/84pHQfSp50RcuUel2'
   },
   partners: {
+    active: true,
     title: 'Partenaires',
     description: 'Retrouvez la liste de tous nos partenaires'
   },
   tournaments: {
+    active: true,
     title: 'Tournois',
     description: 'Tous les tournois de l\'Atlantic LAN 2018'
   },
-  infos: {
+  info: {
+    active: true,
     title: 'Informations pratiques',
     description: 'Toutes les informations sur cet évènement'
   },
   tickets: {
     title: 'Billeterie',
     description: 'Achetez vos places pour l\'Atlantic LAN 2018'
+  },
+  contact: {
+    active: true,
+    pageId: 6
+  },
+  press: {
+    active: false,
+    pageId: 7
+  },
+  legals: {
+    active: true,
+    pageId: 8
   },
   social: {
     twitter: 'https://twitter.com/AtlanticLan17',
